7f59daecb721970777f94fc997a7cfbd05885417
Himanshu9097 <himanshu748808@gmail.com>
2026-07-25 15:21:57 +0530

    Configure Vercel deployment

--- server/server.js
+++ server/server.js
@@ -50,7 +50,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
-// Restart nodemon
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  });
+}
+
+export default app;
